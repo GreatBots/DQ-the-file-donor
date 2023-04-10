5dd1e50a5fb24a494d9e636fa2f473f8adc130dc
Vivek Tp <Vivektvp1@gmail.com>
2023-04-10 20:10:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,9 +50,8 @@ async def start(client, message):
                     InlineKeyboardButton('Join Updates Channel', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
